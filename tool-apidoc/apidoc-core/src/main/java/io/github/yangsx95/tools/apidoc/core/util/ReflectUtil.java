43781e91ac256460512578b201862e3ebe6f303e
杨顺翔 <yangsx95@qq.com>
2024-01-16 10:20:30 +0800

    feat(1.1-SNAPSHOT): 增加对get方法的支持

---
+++ tool-apidoc/apidoc-core/src/main/java/io/github/yangsx95/tools/apidoc/core/util/ReflectUtil.java
@@ -0,0 +1,94 @@
+package io.github.yangsx95.tools.apidoc.core.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ReflectUtil {
+
+    public static boolean hasGetter(Class<?> clazz) {
+        if (clazz == null) {
+            return false;
+        }
+
+        if (clazz.isEnum()) {
+            return false;
+        }
+
+        if (clazz.isArray()) {
+            return false;
+        }
+
+        if (clazz.isAnnotation()) {
+            return false;
+        }
+
+        if (clazz.isPrimitive()) {
+            return false;
+        }
+
+        if (clazz.isSynthetic()) {
+            return false;
+        }
+
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (isGetterMethod(method) && !"getClass".equals(method.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isGetterMethod(Method method) {
+        return method.getName().startsWith("get")
+                && !Modifier.isStatic(method.getModifiers())
+                && method.getParameterCount() == 0
+                && method.getReturnType() != void.class
+                && method.getReturnType() != Void.class
+                && !Objects.equals(method.getName(), "getClass");
+    }
+
+    public static List<BeanProperty> getBeanProperties(Class<?> clazz) {
+        if (Objects.isNull(clazz)) {
+            throw new NullPointerException("class can not null!");
+        }
+
+        // 至少拥有一个getter 才算是一个bean
+        if (!hasGetter(clazz)) {
+            return Collections.emptyList();
+        }
+
+        return Arrays.stream(clazz.getMethods())
+                .filter(ReflectUtil::isGetterMethod)
+                .map(m -> {
+                    String fieldName = getFieldNameFromGetterMethod(m);
+                    Field field;
+                    try {
+                        field = clazz.getField(fieldName);
+                    } catch (NoSuchFieldException ignored) {
+                        field = null;
+                    }
+                    return new BeanProperty(field, m, null);
+                })
+                .collect(Collectors.toList());
+    }
+
+    public static String getFieldNameFromGetterMethod(Method method) {
+        if (!isGetterMethod(method)) {
+            throw new IllegalArgumentException("this method is not a getter");
+        }
+
+        String methodName = method.getName();
+        String fieldName = methodName.substring(3); // 去除 "get"，获取剩余部分
+        fieldName = fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1); // 将首字母转换为小写
+        return fieldName;
+    }
+
+
+}
