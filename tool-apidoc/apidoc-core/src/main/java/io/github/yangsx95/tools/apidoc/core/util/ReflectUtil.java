096f32bd25ba87b04aa6cf3341381cb99e9f7c68
杨顺翔 <yangsx95@qq.com>
2024-02-02 11:50:03 +0800

    feat(1.5-SNAPSHOT): 没有获取父类字段

--- tool-apidoc/apidoc-core/src/main/java/io/github/yangsx95/tools/apidoc/core/util/ReflectUtil.java
+++ tool-apidoc/apidoc-core/src/main/java/io/github/yangsx95/tools/apidoc/core/util/ReflectUtil.java
@@ -1,12 +1,12 @@
 package io.github.yangsx95.tools.apidoc.core.util;
 
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.ArrayUtil;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReflectUtil {
@@ -65,20 +65,39 @@ public class ReflectUtil {
             return Collections.emptyList();
         }
 
+        // 获取所有的字段
+        Field[] fields = getFieldsDirectly(clazz, true);
+        if (fields.length == 0) {
+            return Collections.emptyList();
+        }
+
+        Map<String, Field> fieldMap = new HashMap<>();
+        for (Field field : fields) {
+            fieldMap.put(field.getName(), field);
+        }
+
         return Arrays.stream(clazz.getMethods())
                 .filter(ReflectUtil::isGetterMethod)
-                .map(m -> {
-                    String fieldName = getFieldNameFromGetterMethod(m);
-                    Field field;
-                    try {
-                        field = clazz.getDeclaredField(fieldName);
-                    } catch (NoSuchFieldException ignored) {
-                        field = null;
-                    }
-                    return new BeanProperty(field, m, null);
-                })
+                .map(m -> new BeanProperty(fieldMap.get( getFieldNameFromGetterMethod(m)), m, null))
                 .collect(Collectors.toList());
     }
+
+    public static Field[] getFieldsDirectly(Class<?> beanClass, boolean withSuperClassFields) throws SecurityException {
+        Assert.notNull(beanClass);
+        Field[] allFields = null;
+
+        for (Class<?> searchType = beanClass; searchType != null; searchType = withSuperClassFields ? searchType.getSuperclass() : null) {
+            Field[] declaredFields = searchType.getDeclaredFields();
+            if (null == allFields) {
+                allFields = declaredFields;
+            } else {
+                allFields = ArrayUtil.append(allFields, declaredFields);
+            }
+        }
+
+        return allFields;
+    }
+
 
     public static String getFieldNameFromGetterMethod(Method method) {
         if (!isGetterMethod(method)) {
@@ -90,6 +109,5 @@ public class ReflectUtil {
         fieldName = fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1); // 将首字母转换为小写
         return fieldName;
     }
-
 
 }
